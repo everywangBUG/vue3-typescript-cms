6abf3168077c46ea2af81fa5756b964386a4ae7e
everywang <wz15979455211@163.com>
2023-05-06 23:18:29 +0800

    重构解决丢失路由的bug-优雅永不过时

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,8 @@ import App from '@/App.vue'
 import { createApp } from 'vue'
 import '@/assets/css/index.less'
 import router from './router/route'
-import pinia from './store'
 import registerIcons from './global/registerIcons'
-import useLoginStore from './store/login/login'
+import registerStore from './store'
 
 // 1.对ElMessage和ElLoading全局注册引入element-plus的所有样式
 // import 'element-plus/dist/index.css'
@@ -15,10 +14,8 @@ import useLoginStore from './store/login/login'
 
 const app = createApp(App)
 app.use(registerIcons)
-app.use(pinia)
-// 在使用pinia下面完成store中的加载本地的所有数据
-const loginStore = useLoginStore()
-loginStore.loadLocalCacheAction()
+// 使用刷新时加载路由的导出函数
+app.use(registerStore)
 
 // 等本地的路由全部加载完毕，再加载路由
 app.use(router)
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,15 @@
 import { createPinia } from 'pinia'
+import type { App } from 'vue'
+import useLoginStore from './login/login'
 
 const pinia = createPinia()
 
-export default pinia
+function registerStore(app: App<Element>) {
+  // 1.首先use(pinia)
+  app.use(pinia)
+  // 2.在使用pinia下面完成store中的加载本地的所有数据
+  const loginStore = useLoginStore()
+  loginStore.loadLocalCacheAction()
+}
+
+export default registerStore
